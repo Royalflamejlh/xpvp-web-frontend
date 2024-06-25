6b3b2fb99379b26495543bbfe177ce8049dfd5e7
Rooyal <49204095+Royalflamejlh@users.noreply.github.com>
2024-06-25 11:54:36 -0400

    Remove tags page

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/kits', title: 'Kits' },
-  { href: '/tags', title: 'Delete This' },
   { href: '/projects', title: 'Stats' },
   { href: '/about', title: 'About' },
 ]
